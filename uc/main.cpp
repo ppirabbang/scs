631ff0cc7536a0933f3ad9dde46bab6385109be9
gilgil <gilgil@gilgil.net>
2020-05-30 02:26:53 +0900

    First commit

---
+++ uc/main.cpp
@@ -0,0 +1,89 @@
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <iostream>
+#include <thread>
+
+using namespace std;
+
+void usage() {
+	cout << "syntax: uc <ip> <port>\n";
+	cout << "sample: uc 127.0.0.1 1234\n";
+}
+
+struct Param {
+	struct in_addr ip{0};
+	uint16_t port{0};
+
+	bool parse(int argc, char* argv[]) {
+		if (argc != 3) return false;
+		int res = inet_pton(AF_INET, argv[1], &ip);
+		switch (res) {
+			case 1: break;
+			case 0: cerr << "not a valid network address\n"; return false;
+			case -1: perror("inet_pton"); return false;
+		}
+		port = stoi(argv[2]);
+		return true;
+	}
+} param;
+
+void recvThread(int sd) {
+	static const int BUFSIZE = 65536;
+	char buf[BUFSIZE];
+	while (true) {
+		struct sockaddr_in addr;
+		socklen_t len = sizeof(addr);
+		memset(&addr, 0, sizeof(addr));
+		ssize_t res = recvfrom(sd, buf, BUFSIZE - 1, 0, (struct sockaddr*)&addr, &len);
+		cout << "recvfrom return " << res << endl; // gilgil temp
+		if (res == 0 || res == -1) {
+			fprintf(stderr, "recvfrom return %ld\n", res);
+			perror("recvfrom");
+			break;
+		}
+		buf[res] = '\0';
+		cout << buf << endl;
+	}
+	exit(0);
+}
+
+int main(int argc, char* argv[]) {
+	if (!param.parse(argc, argv)) {
+		usage();
+		return -1;
+	}
+
+	int sd = socket(AF_INET, SOCK_DGRAM, 0);
+	if (sd == -1) {
+		perror("socket");
+		return -1;
+	}
+
+	struct sockaddr_in addr;
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(param.port);
+	addr.sin_addr = param.ip;
+	memset(&addr.sin_zero, 0, sizeof(addr.sin_zero));
+
+	//thread t(recvThread, sd);
+	//t.detach();
+	bool isFirst = true;
+	while (true) {
+		string s;
+		getline(cin, s);
+		ssize_t res = sendto(sd, s.c_str(), s.size(), 0, (struct sockaddr*)&addr, sizeof(addr));
+		if (res == 0 || res == -1) {
+			fprintf(stderr, "sendto return %ld\n", res);
+			perror("sendto");
+			break;
+		}
+		if (isFirst) {
+			isFirst = false;
+			thread* t = new thread(recvThread, sd);
+			t->detach();
+		}
+	}
+	close(sd);
+}
---
+++ us/main.cpp
@@ -0,0 +1,87 @@
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <iostream>
+#include <thread>
+
+using namespace std;
+
+void usage() {
+	cout << "syntax: us <port>\n";
+	cout << "sample: us [-e] 1234\n";
+}
+
+struct Param {
+	bool echo{false};
+	uint16_t port{0};
+
+	bool parse(int argc, char* argv[]) {
+		if (argc < 2) return false;
+		bool portExist = false;
+		for (int i = 1; i < argc; i++) {
+			if (strcmp(argv[i], "-e") == 0) {
+				echo = true;
+				continue;
+			}
+			port = stoi(argv[i]);
+			portExist = true;
+		}
+		return portExist;
+	}
+} param;
+
+void recvThread(int sd) {
+	static const int BUFSIZE = 65536;
+	char buf[BUFSIZE];
+	while (true) {
+		struct sockaddr_in addr;
+		socklen_t len = sizeof(addr);
+		memset(&addr, 0, sizeof(addr));
+		ssize_t res = recvfrom(sd, buf, BUFSIZE - 1, 0, (struct sockaddr*)&addr, &len);
+		if (res == 0 || res == -1) {
+			fprintf(stderr, "recvfrom return %ld\n", res);
+			perror("recv");
+			break;
+		}
+		buf[res] = '\0';
+		cout << buf << endl;
+		if (param.echo) {
+			res = sendto(sd, buf, res, 0, (struct sockaddr*)&addr, sizeof(addr));
+			if (res == 0 || res == -1) {
+				fprintf(stderr, "sendto return %ld\n", res);
+				perror("sendto");
+				break;
+			}
+		}
+	}
+}
+
+int main(int argc, char* argv[]) {
+	if (!param.parse(argc, argv)) {
+		usage();
+		return -1;
+	}
+
+	int sd = socket(AF_INET, SOCK_DGRAM, 0);
+	if (sd == -1) {
+		perror("socket");
+		return -1;
+	}
+
+	struct sockaddr_in addr;
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(param.port);
+	addr.sin_addr.s_addr = INADDR_ANY;
+	memset(&addr.sin_zero, 0, sizeof(addr.sin_zero));
+
+	int res = bind(sd, (struct sockaddr*)&addr, sizeof(addr));
+	if (res == -1) {
+		perror("bind");
+		return -1;
+	}
+
+	thread t(recvThread, sd);
+	t.join();
+	close(sd);
+}
