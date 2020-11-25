6744ac64df795276de04109d9da20401302e718c
gilgil <gilgil@gilgil.net>
2020-11-25 20:38:04 +0900

    Change binary files output into bin directory

--- tc/tc.cpp
+++ tc/tc.cpp
@@ -86,6 +86,7 @@ int main(int argc, char* argv[]) {
 
 	thread t(recvThread, sd);
 	t.detach();
+
 	while (true) {
 		string s;
 		getline(cin, s);
--- tc/tc.pro
+++ tc/tc.pro
@@ -3,5 +3,6 @@ CONFIG += console c++11
 CONFIG -= app_bundle
 CONFIG -= qt
 LIBS += -pthread
+DESTDIR = $${PWD}/../bin
 SOURCES += \
     tc.cpp
